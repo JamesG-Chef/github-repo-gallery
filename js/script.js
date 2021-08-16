3d0fe2483b3207bd4a6be8e7e6f244ee6328f548
James_G <jamesg.chef@gmail.com>
2021-08-16 10:54:48 +0100

    Completed project

--- js/script.js
+++ js/script.js
@@ -8,7 +8,13 @@ const repoList = document.querySelector(".repo-list");
 const repoContainer = document.querySelector(".repos");
 // Create a global variable that selects the secion with a class of "repo-data";
 const repoData = document.querySelector(".repo-data");
+// Create a global variable to select the 'back to repo gallery' button
+const returnButton = document.querySelector(".view-repos")
+// Create a global variable to select the input with the 'Search by name' placeholder
+const filterInput = document.querySelector(".filter-repos")
 
+
+// 	FETCH USER INFO DATA FROM THE GITHUB API:
 
 // Create an async function to fetch data from your github profile using the github API
 const gitUserInfo = async function () {
@@ -25,6 +31,8 @@ const gitUserInfo = async function () {
 // Call your function to see your results.
 gitUserInfo();
 
+
+// CREATE A FUNCTION TO DISPLAY THE USER INFO DATA FROM THE API:
 
 const displayUserInfo = function (data) {
 	// create a new div and give it a class of “user-info”. 
@@ -50,7 +58,7 @@ const displayUserInfo = function (data) {
 };
 
 
-
+// FETCH REPO DATA FROM THE GITHUB API:
 
 // create and name a new async function to fetch your repos
 const fetchRepos = async function () {
@@ -64,9 +72,14 @@ const fetchRepos = async function () {
 	displayRepos(data);
 };
 
+
+// CREATE A FUNCTION TO DISPLAY THE REPO DATA FROM THE API:
+
 // create and name a function to display information about each repo
 // Use 'repos' as a parameter so that the function accepts the data returned from your last API call
 const displayRepos = function (repos) {
+	// At the top of the function that displays all your repos, show the filterInput element
+	filterInput.classList.remove("hide");
 	// Inside the function, loop and create a list item for each repo 
 	for (const repo of repos) { // For each element (repo), within the array (repos)
 		const repoItem = document.createElement("li"); // Create the list item
@@ -78,6 +91,9 @@ const displayRepos = function (repos) {
 		repoList.append(repoItem);
 	}
 };
+
+
+// CREATE A CLICK EVENT FOR SELECTNG AN ITEM FROM THE REPO LIST:
 
 // Create an event listener called 'repoList' for a click event on the unorderedlist with a class of "repo-list", 
 // pass the event (e) in the callback function
@@ -94,6 +110,9 @@ repoList.addEventListener("click", function (e) {
 
 	}
 });
+
+
+// FETCH SPECIFIC REPO INFORMATION FROM THE API USING THE REPONAME:
 
 // create and name an async function to get specific repo information that accepts repoName as a parameter. 
 
@@ -122,6 +141,9 @@ const getRepoInfo = async function (repoName) {
 	displayRepoInfo(repoInfo, languages);
 };
 
+
+// CREATE A FUNCTION TO DISPLAY SPECIFIC REPO INFORMATION:
+
 // Create and name a new function to display the specific repo information. 
 // The function should accept two parameters:  repoInfo and languages.
 const displayRepoInfo = function (repoInfo, languages) {
@@ -145,7 +167,53 @@ const displayRepoInfo = function (repoInfo, languages) {
     repoData.classList.remove("hide");
     // Hide the element with the class of "repos"
     repoContainer.classList.add("hide");
+    // remove the class of “hide” from the Back to Repo Gallery button. 
+    returnButton.classList.remove("hide");
 }
+
+// CREATE A CLICK EVENT FUNCTION FOR THE 'BACK TO REPO GALLERY' BUTTON:
+
+// create a click event listener attached to your variable that points to the Back to Repo Gallery button
+returnButton.addEventListener("click", function () {
+	// unhide (display) the section with the class of “repos”
+	const repos = document.querySelector(".repos");
+	repos.classList.remove("hide");
+	// Add the “hide” class to the section where the individual repo data will appear
+	repoData.classList.add("hide");
+	// Add the “hide” class to the 'Back to Repo Gallery button' itself. 
+	returnButton.classList.add("hide");
+
+});
+
+// CREATE A DYNAMIC SEARCH BOX:
+
+// Attach an "input" event listener to filterInput. Pass the event (e) the callback function
+filterInput.addEventListener("input", function (e) {
+	// create a variable to capture the value of the search text
+	const searchText = e.target.value;
+	// Log out the variable to check it's capturing the data
+	console.log(searchText);
+	// Create a variable called repos to select ALL elements on the page with a class of “repo”
+	const repos = document.querySelectorAll(".repo");
+	// Create a variable and assign it to the lowercase value of the search text
+	const lowercaseSearch = searchText.toLowerCase();
+	// Loop through each repo inside your repos element. 
+for (const repo of repos) {
+	// Inside the loop, create a variable and assign it to the lowercase value of the innerText. of each repo.
+	const lowerCaseValue = repo.innerText.toLowerCase();
+	// If the repo contains the text, show it. If it doesn’t contain the text, hide the repo.
+		if (lowerCaseValue.includes(lowercaseSearch)) {
+				repo.classList.remove("hide");
+			} else {
+				repo.classList.add("hide");
+			}
+		}
+});
+
+
+
+
+
 
 
 
